f066fceb2a693b2e3e403fb219d2fcb56362ce21
Felipe Elias Philipp <felipeelias@gmail.com>
2011-11-28 11:27:40 +0100

    Add sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,13 @@
 25.times do
   Profile.create! :name => Faker::Name.name, :email => Faker::Internet.email, :username => Faker::Internet.user_name
 end
+
+User.create! :email => "user@example.com", :password => "doorkeeper", :password_confirmation => "doorkeeper"
+
+app = Application.create! :name => "Doorkeeper Sinatra Client", :redirect_uri => "http://doorkeeper-sinatra.herokuapp.com/callback"
+
+puts "Application: "
+puts "name: #{app.name}"
+puts "redirect_uri: #{app.redirect_uri}"
+puts "uid: #{app.uid}"
+puts "secret: #{app.secret}"
