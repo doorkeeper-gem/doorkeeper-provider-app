6fbb87859a492fcce9e90f28c657fb1a24bcc83e
Felipe Elias Philipp <felipeelias@gmail.com>
2011-11-28 10:55:04 +0100

    Install devise with user model

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :encryptable, :confirmable, :lockable,
+  # :timeoutable, :trackable, :registerable, :recoverable and :omniauthable
+  devise :database_authenticatable, :rememberable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me
+end
--- config/application.rb
+++ config/application.rb
@@ -44,5 +44,8 @@ module DoorkeeperProvider
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Force the application to not access the DB or load models when precompiling your assets.
+    config.assets.initialize_on_precompile = false
   end
 end
