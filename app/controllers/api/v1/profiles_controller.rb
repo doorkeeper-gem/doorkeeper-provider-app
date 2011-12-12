bf277620584cf6f82a5709ef3abd1194723e0b6c
Felipe Elias Philipp <felipeelias@gmail.com>
2011-12-12 11:11:38 +0100

    Limit index list and add create action

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -5,7 +5,11 @@ module Api::V1
     respond_to :json
 
     def index
-      respond_with Profile.all
+      respond_with Profile.limit(5).order('updated_at desc')
+    end
+
+    def create
+      Profile.create!(params[:profile])
     end
   end
 end
