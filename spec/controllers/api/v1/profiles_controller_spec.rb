98cf6f707995fd8d9b55621d33d44626f2f056e9
Felipe Elias Philipp <felipeelias@gmail.com>
2012-02-05 15:24:46 +0100

    Add profiles controller spec

---
+++ spec/controllers/api/v1/profiles_controller_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+
+describe Api::V1::ProfilesController do
+  describe 'GET #index' do
+    let(:token) { stub(:token => "some-token", :accessible? => true) }
+
+    before do
+      controller.stub(:doorkeeper_token) { token }
+    end
+
+    it 'responds with 200' do
+      get :index, :format => :json
+      response.status.should eq(200)
+    end
+
+    it 'returns recent profiles as json' do
+      Profile.should_receive(:recent) { [] }
+      get :index, :format => :json
+      response.body.should == [].to_json
+    end
+
+    it 'responds with 401 when unauthorized' do
+      token.stub :accessible? => false
+      get :index, :format => :json
+      response.status.should eq(401)
+    end
+  end
+
+  describe 'POST #create (with scopes)' do
+    let(:token) do
+      stub :token => "some-token", :accessible? => true, :scopes => [:write]
+    end
+
+    before do
+      controller.stub(:doorkeeper_token) { token }
+    end
+
+    it 'returns the user as json' do
+      Profile.should_receive(:create!) { stub_model(Profile) }
+      post :create, :format => :json
+      response.status.should eq(201)
+    end
+  end
+end
