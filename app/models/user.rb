a9155f057d71c97604d0682f25077b10c73ff673
Douglas Lovell <doug@wbreeze.com>
2014-08-27 11:57:03 -0400

    upgrade user model and data migration for Devise 2.2.8

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me
+  # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable,
   # :timeoutable, :trackable, :registerable, :recoverable and :omniauthable
   devise :database_authenticatable, :rememberable, :validatable
