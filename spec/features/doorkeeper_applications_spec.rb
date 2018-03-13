1235eb56a50cabf56f07f1bbc115a4e6c79ae542
Sean Carroll <sfcarroll@gmail.com>
2018-03-13 16:41:34 +0100

    fix issues with new application create

--- spec/features/doorkeeper_applications_spec.rb
+++ spec/features/doorkeeper_applications_spec.rb
@@ -8,3 +8,16 @@ RSpec.feature "Accessing the OAuth provider page" do
   end
 
 end
+
+RSpec.feature "Create a new application" do
+
+  scenario 'public user' do
+    visit "/oauth/applications/new"
+    expect(page).to have_content 'New application'
+    fill_in "Name", with: 'Sean'
+    fill_in "Redirect URI", with: 'urn:ietf:wg:oauth:2.0:oob'
+    click_on 'Submit'
+    expect(page).to have_content 'Application created.'
+  end
+
+end
