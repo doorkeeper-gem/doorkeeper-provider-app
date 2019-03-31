a53aa3ad6b66e5da8e6be0322488d9dbfbcef4b1
Felipe Philipp <felipeelias@gmail.com>
2019-03-31 16:25:06 +0200

    Require authentication

--- app/controllers/oauth_applications_controller.rb
+++ app/controllers/oauth_applications_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class OauthApplicationsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_application, only: %i[show edit update destroy]
 
   def index
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class ProjectsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_project, only: %i[show edit update destroy]
 
   # GET /projects
