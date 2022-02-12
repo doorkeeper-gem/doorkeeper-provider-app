fd1f4db432cf0a84e0d3da85928232a9ee11edb5
Nikita Bulai <bulaj.nikita@gmail.com>
2022-02-12 13:05:06 +0300

    Update gems

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ Bundler.require(*Rails.groups)
 module DoorkeeperProvider
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 7.0
 
     config.to_prepare do
       # Only Applications list
